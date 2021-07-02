2b503536adb114e643d5f962cac315ea04fb30df
nkwadioo <nkwadioo@gmail.com>
2021-07-02 17:34:43 +0200

    Initial commit

---
+++ src/app/edit-listing-page/edit-listing-page.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { fakeMyListings } from '../fake-data';
+import { Listing } from '../types';
+
+
+@Component({
+  selector: 'app-edit-listing-page',
+  templateUrl: './edit-listing-page.component.html',
+  styleUrls: ['./edit-listing-page.component.css']
+})
+export class EditListingPageComponent implements OnInit {
+  listing: Listing;
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {    
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeMyListings.find( listing =>  listing.id === id);
+  }
+
+  onSubmit(): void {
+    alert('Saving changes to the listing');
+    this.router.navigateByUrl('/my-listings')
+  }
+
+}
